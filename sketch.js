ddaaf04cf4c458075e114c228d30965ea9e19b4b
JacobFin <44954274+JacobFin@users.noreply.github.com>
2019-11-03 16:34:38 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,16 +2,27 @@ var myCanv,myDiv,myButton,myAss1,myAss2,myAss3,myAss4,myAss5,googleDIv;
 var spotifyPlaylist, calendar, capture;
 let newsTitles=[];
 let newsURLs=[];
-let playlistPlayer;
-var myPlaylist = ['OdZrFtIGzfU','pqiJ7krbEDM','Oi7WH7_NO9I'];
+
 var myNews;
 var startedApp;
 let loginButton;
-var tag = document.createElement('script');     
+let button;
+let bleh = false;
+let health = false;
+let mycanv;
+let sq1;
+let sq2;
+let sq3;
+var newsURL = "https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=31ac97faff7841a0beec5fcbeae608c9";
+var myPlaylist = ['OdZrFtIGzfU','pqiJ7krbEDM','Oi7WH7_NO9I'];
+let playlistPlayer;
+  
+
+var tag = document.createElement('script');   
 tag.src = "https://www.youtube.com/iframe_api";
+
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-var newsURL = "https://newsapi.org/v2/top-headlines?country=us&pageSize=5&apiKey=31ac97faff7841a0beec5fcbeae608c9";
 function preload()
 {
     loadJSON(newsURL,gotData);
@@ -26,12 +37,17 @@ function gotData(data)
 }
 
 function setup() {
+    sq3 = square(0,0,0);
+    sq1 = square(400,200,200);
 
     myCanv=createCanvas(1200, 650);
     myCanv.position(0,0);
-    capture = createCapture();
+    capture = createCapture(VIDEO);
     capture.size(600, 325);
     capture.hide();
+    button = createButton("On");
+    button.position(1100,600);
+    button.mousePressed(start);
 
     spotifyPlaylist = createDiv();
     spotifyPlaylist.position(0,550);
@@ -39,10 +55,11 @@ function setup() {
     calendar = createDiv();
     calendar.position(0, 0);
     calendar.html('<iframe src="https://calendar.google.com/calendar/embed?src=yellowtroll77%40gmail.com&ctz=America%2FChicago" style="border: 0" width="300" height="300" frameborder="0" scrolling="no"></iframe>');
+    spotifyPlaylist.hide();
+    calendar.hide();
 }
 
 function draw() {
-    background(100,200,255);
 
     //camera stuff
     translate(width, 0);
@@ -50,50 +67,118 @@ function draw() {
     image(capture, 0, 0, width, height);
     translate(width, 0);
     scale(-1.0, 1.0);
+if(bleh == true)
+    {
+      
+      spotifyPlaylist.show();
+      calendar.show();
+      fill(255,0,0);
+      sq2 = square(1100,450,60,20);
+      fill(51);
+      text("Health",1100,485);
+      textFont("georgia");
+      h = hour();
+      m = minute();
+      mon = month();
+      y = year();
+      d = day();
+      if(h>12)
+      {
+        h= h-12;
     
-if (myNews){
-    var baseX=50;
-   for (var i =0; i<5;i++){
-       newsTitles[i]=myNews.articles[i].title;
-       newsURLs[i]=myNews.articles[i].url;
-   }
-   myAss1 = createA(newsURLs[0],newsTitles[0]);
-   myAss1.position(0,400);
-   myAss1.size(250,100);
+        if(m<10)
+          {
+            textSize(32);
+            text(h +':0'+ m +' PM', 1000,50);
+            textSize(16);
+            text(mon + '/' + d + '/' + y, 1027, 70);
+          }
+        else
+        {
+          textSize(32);
+          text(h +':'+ m + ' PM', 1000,50);
+          textSize(16);
+          text(mon + '/' + d + '/' + y,1027,70);
+      
+        }
+    }
+    else
+    {
+      if(m<10)
+      {
+        textSize(32);
+        text(h +':0'+ m +' AM', 1000,50);
+        textSize(16);
+        text(mon + '/' + d + '/' + y,1027,70);
+      }
+      else
+      {
+        textSize(32);
+        text(h +':'+ m + ' AM', 1000,50);
+        textSize(16);
+        text(mon + '/' + d + '/' + y,1027,70);
+      }
+    }
+    if(health == true)
+    {
+      sq3 = square(1000,400,195);
+      fill(255,255,255);
+      text("X",1000,412);
+      text("Calories Eaten Today: 2850",1000,430);
+      text("Steps walked Today: 5,285",1000,445);
+      text("sleep last night: 6 hours",1000,460);
+      text("Weekly Calories: 20,500",1000,530);
+      text("Weekly Steps: 37,836",1000,545);
+      text("Weekly Sleep: 41 hours",1000,560);
+    }
+    if(health == false)
+    {
+      sq3 = square(0,0,0);
+    }
+    
+    if (myNews){
+        var baseX=50;
+       for (var i =0; i<5;i++){
+           newsTitles[i]=myNews.articles[i].title;
+           newsURLs[i]=myNews.articles[i].url;
+       }
+       myAss1 = createA(newsURLs[0],newsTitles[0]);
+       myAss1.position(0,400);
+       myAss1.size(250,100);
 
 
-   myAss2 = createElement('a',newsTitles[1]);
-   myAss2.position(200,400);
-   myAss2.size(200,100);
-   myAss2.attribute('href',newsURLs[1]);
+       myAss2 = createElement('a',newsTitles[1]);
+       myAss2.position(200,400);
+       myAss2.size(200,100);
+       myAss2.attribute('href',newsURLs[1]);
 
 
 
-   myAss3 = createElement('a',newsTitles[2]);
-   myAss3.attribute('href',newsURLs[2]);
-   myAss3.position(400,400);
-   myAss3.size(200,100);
+       myAss3 = createElement('a',newsTitles[2]);
+       myAss3.attribute('href',newsURLs[2]);
+       myAss3.position(400,400);
+       myAss3.size(200,100);
 
 
-   myAss4 = createElement('a',newsTitles[3]);
-   myAss4.attribute('href',newsURLs[3]);
-   myAss4.position(600,400);
-   myAss4.size(200,100);
+       myAss4 = createElement('a',newsTitles[3]);
+       myAss4.attribute('href',newsURLs[3]);
+       myAss4.position(600,400);
+       myAss4.size(200,100);
 
 
-    myAss5 = createElement('a',newsTitles[4]);
-    myAss5.attribute('href',newsURLs[4]);
-    myAss5.position(800,400);
-    myAss5.size(200,100);
+        myAss5 = createElement('a',newsTitles[4]);
+        myAss5.attribute('href',newsURLs[4]);
+        myAss5.position(800,400);
+        myAss5.size(200,100);
 
-    textSize(20);
-    text('News',0,365,200,450);
-    fill(255);
-    text('Playlist',675,130,1000,152);
+        textSize(20);
+        text('News',0,365,200,450);
+        fill(255);
+        text('Playlist',675,130,1000,152);
+    }
 
 
 
-}
 myDiv=createDiv();
 myDiv.size(320,195);
 myDiv.position(680,152);
@@ -106,6 +191,7 @@ playlistPlayer = new YT.Player('YTDiv',{
 'onReady': onPlayerReady
     }
 });
+    }
 }
 
 
@@ -122,4 +208,36 @@ myButton.attribute('onClick','getNews(articleHolder)')
     // 4. The API will call this function when the video player is ready
 function onPlayerReady(event) {
 event.target.cuePlaylist(myPlaylist);
+}
+function start()
+{
+  bleh = true;
+  button = createButton("Off")
+  button.position(1100,600);
+  button.mousePressed(stop);
+}
+function stop()
+{
+  bleh = false
+  button = createButton("On");
+  button.position(1100,600);
+  button.mousePressed(start);
+  spotifyPlaylist.hide();
+  calendar.hide();
+}
+function mousePressed()
+{
+  if(mouseX>1050 && mouseX<1150 && mouseY>390 && mouseY<510)
+  {
+    if(health == false)
+    {
+      health = true;
+    }
+  }
+  if (mouseX>950 && mouseX<1050 && mouseY>400 && mouseY<430)
+    {
+      health = false;
+    }
+    
+  
 }
